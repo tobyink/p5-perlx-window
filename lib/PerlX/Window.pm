8f70a42995d641ec7981f3dcbbc21bafe5368c46
Toby Inkster <mail@tobyinkster.co.uk>
2014-09-16 14:13:14 +0100

    bump version; changelog

--- lib/PerlX/Window.pm
+++ lib/PerlX/Window.pm
@@ -5,7 +5,7 @@ use warnings;
 package PerlX::Window;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.003';
+our $VERSION   = '0.004';
 
 use Data::Alias;
 use Exporter::Shiny (our @EXPORT = qw( window window_pos ));
